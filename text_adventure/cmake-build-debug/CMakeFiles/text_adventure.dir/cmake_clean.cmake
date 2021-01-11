b475964e30154d24d913eaa4bf3f5c39d3ee2919
Marvin <marvin.kunst@gmx.de>
2021-01-11 19:16:52 +0100

    fix

---
+++ text_adventure/cmake-build-debug/CMakeFiles/text_adventure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text_adventure.dir/main.cpp.obj"
+  "text_adventure.exe"
+  "text_adventure.exe.manifest"
+  "text_adventure.lib"
+  "text_adventure.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/text_adventure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
